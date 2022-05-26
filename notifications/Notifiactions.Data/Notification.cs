08272c80276b63ec08b335fdda9302319a0c1829
Patryk.Dyminski <patryk-dym@o2.pl>
2022-05-26 22:44:25 +0200

    Json schema in notifications module

--- notifications/Notifiactions.Data/Notification.cs
+++ notifications/Notifiactions.Data/Notification.cs
@@ -12,3 +12,13 @@ public class Notification
   public bool FirstNotifSent { get; set; }
   public bool SecondNotifSent { get; set; }
 }
+
+public class NotificationSchema
+{
+  public string LastName { get; set; }
+  public string FirstName { get; set; }
+  public string Email { get; set; }
+  public string EventName { get; set; }
+  public DateTime EventDate { get; set; }
+  public string EventLocation { get; set; }
+}
